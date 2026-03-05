f8108cfc29daad91695bdb0a309fd2b0392e80c2
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2026-03-05 08:41:19 -0300

    exercicio1

---
+++ exercicio1_enunciado.py
@@ -0,0 +1,32 @@
+"""
+Exercício 1:
+*Antecessor e sucessor de um número:
+-> Escreva um programa em python que leia um número e represente o número antecessor e
+sucessor desse número que foi lido, utilizando operadores de atribuição.
+
+* Média de 4 notas:
+-> Escreva um programa em Python que leia quatro números e calcule a média entre esses
+números
+"""
+
+num = int(input("Digite um número: "))
+numAntecessor = num -1
+numSucessor = num + 1
+
+print(f"Número antecessor: ", numAntecessor)
+print(f"Numéro sucessor: ", numSucessor)
+
+
+print("------------------------")
+print("\n")
+
+num1 = int(input("Digite o primeiro número: "))
+num2= int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+num4 = int(input("Digite o quarto número: "))
+
+media = (num1 + num2 + num3 + num4) / 4
+print(f"A média dos números {num1} + {num2} + {num3} + {num4} é: ", media)
+
+
+
