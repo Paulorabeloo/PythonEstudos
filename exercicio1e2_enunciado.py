817d7aba491df096b7f42aee3c57de32143e2953
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2026-03-06 08:26:41 -0300

    Ex 1 e 2

--- exercicio1e2_enunciado.py
+++ exercicio1e2_enunciado.py
@@ -4,6 +4,7 @@ Exercício 1:
 -> Escreva um programa em python que leia um número e represente o número antecessor e
 sucessor desse número que foi lido, utilizando operadores de atribuição.
 
+Ex 2:
 * Média de 4 notas:
 -> Escreva um programa em Python que leia quatro números e calcule a média entre esses
 números
@@ -17,7 +18,7 @@ print(f"Número antecessor: ", numAntecessor)
 print(f"Numéro sucessor: ", numSucessor)
 
 
-print("------------------------")
+print("----------EX2------------")
 print("\n")
 
 num1 = float(input("Digite o primeiro número: "))
