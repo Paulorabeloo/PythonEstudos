c0df9588798340305f7c11c2ef7e61ef09635540
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2026-04-27 07:05:46 -0300

    Reorganizando estrutura em pastas

---
+++ 2-Modulos/1-modulos.py
@@ -0,0 +1,8 @@
+import calc
+from calc import div
+
+#calc é um módulo
+print(calc.sum(5 , 10))
+print(calc.sub(10, 6))
+print(calc.mult(10, 6))
+print(div(10, 20))
--- README.md
+++
@@ -1,1 +0,0 @@
-<img width="304" height="409" alt="image" src="https://github.com/user-attachments/assets/79a2ad9f-27c7-46ad-b173-f5df08ddbc81" />
